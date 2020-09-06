54a250acde9efee2d64b9bb005c7f25d59c63845
patricklapgar <patricklapgardeveloper@gmail.com>
2020-09-06 12:16:32 -0500

    Created Node class

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from queue import PriorityQueue
 # Set up window display
 # Window width
 WIDTH = 800
-WIN = pygame.display.set_mode((WIDTH, WIDTH))
+WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A * Path Finding Visualizer")
 
 # All the colors to be used in this project
@@ -36,3 +36,41 @@ class Node:
 
         def get_position(self):
             return self.row, self.col
+
+        # Red nodes
+        def is_closed(self):
+            return self.color == RED
+
+        def is_open(self):
+            return self.color == GREEN
+
+        def is_barrier(self):
+            return self.color == BLACK
+
+        def is_start(self):
+            return self.color == ORANGE
+
+        def is_end(self):
+            return self.color == TURQUOISE
+
+        def reset(self):
+            self.color == WHITE
+
+        def make_closed(self):
+            self.color = RED
+
+        def make_open(self):
+            self.color = GREEN
+
+        def make_barrier(self):
+            self.color = BLACK
+
+        def make_end(self):
+            self.color = TURQUOISE
+
+        def make_path(self):
+            self.color = PURPLE
+
+        # Draw the nodes onto the grid
+        def draw(self, win):
+            pygame.draw.rect(win, self.color, ())
