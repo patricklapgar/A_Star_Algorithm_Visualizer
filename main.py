9785d91ee0e8954bbd86db7cf161500d735f645f
patricklapgar <patricklapgardeveloper@gmail.com>
2020-09-20 22:18:19 -0500

    Finished algorithm function

--- main.py
+++ main.py
@@ -122,6 +122,39 @@ def algorithm(draw, grid, start, end):
     # Set that keeps track of everything in the priority queue
     open_set_hash = {start}
 
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        # Current node being looked at
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            # TODO: Make path
+            return True
+        
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + heuristic(neighbor.get_position(), end.get_position())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+        
+        draw()
+
+        if current != start:
+            current.make_closed()
+            
+    return False
+
 # Grid function
 def make_grid(rows, width):
     grid = []
